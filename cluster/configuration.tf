c721464cabc49d4ef764ec8077fea14d41fe85bb
MakariosNassef <makarios059@gmail.com>
2023-01-27 01:49:30 +0200

    add ingress services

--- cluster/configuration.tf
+++ cluster/configuration.tf
@@ -6,15 +6,5 @@ resource "google_service_account" "gke-cluster-service" {
 resource "google_project_iam_member" "example_binding" {
   project = "iti-makarios"
   role    = "roles/storage.objectViewer"
-  #role    = "roles/container.admin"
   member  = "serviceAccount:${google_service_account.gke-cluster-service.email}"
 }
-
-# resource "google_project_iam_member" "obj_example_binding" {
-#   project = "iti-makarios"
-#   role    = "roles/storage.objectAdmin"
-#   member  = "serviceAccount:${google_service_account.gke-cluster-service.email}"
-# }
-
-
-
--- network/main.tf
+++ network/main.tf
@@ -31,19 +31,6 @@ resource "google_compute_firewall" "management_subnet_firewall" {
   }
 }
 
-resource "google_compute_firewall" "restricted_subnet_firewall" {
-  name    = "restricted-subnet-firewall"
-  project = "iti-makarios"
-  network = google_compute_network.vpc.id
-  direction = "EGRESS"
-  source_ranges = [google_compute_subnetwork.restricted.ip_cidr_range]
-  priority = 100
-  deny {
-    protocol = "all"
-    #ports    = ["22", "80"]
-  }
-}
-
 resource "google_compute_address" "nat_ip" {
   name = "nat-ip"
   region = "asia-east2"
@@ -56,7 +43,7 @@ resource "google_compute_instance" "private-management-vm" {
  
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
     }
   }
   metadata = {
@@ -76,24 +63,23 @@ resource "google_compute_instance" "private-management-vm" {
     ]
   }
   metadata_startup_script = <<-EOF
-    sudo apt update -y
-    sudo apt-get install apt-transport-https ca-certificates gnupg
+    sudo apt-get install  -y apt-transport-https ca-certificates gnupg
     echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-    sudo apt-get update && sudo apt-get install google-cloud-cli -y
+    sudo apt-get update && sudo apt-get install google-cloud -y
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
     chmod +x kubectl
     mkdir -p ~/.local/bin
     mv ./kubectl ~/.local/bin/kubectl
+    kubectl version --client
     sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
-    echo "done" > done.txt
-    sudo USE_GKE_GCLOUD_AUTH_PLUGIN: True
+    # sudo USE_GKE_GCLOUD_AUTH_PLUGIN: True
+    # gcloud container clusters get-credentials private-cluster --zone asia-east2-a --project iti-makarios
   EOF
 }
-
-
 
 resource "google_compute_router" "router" {
     name    = "nat-router"
